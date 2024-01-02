0972ccec91840314c1b86e6c078075fa3a98c4bf
소민환 <sby4006@i-gns.co.kr>
2024-01-02 17:50:06 +0900

    20240102 SMH [WebClient] 메서드 추가

--- src/main/java/kr/co/maple/module/main/service/MainService.java
+++ src/main/java/kr/co/maple/module/main/service/MainService.java
@@ -1,23 +1,17 @@
 package kr.co.maple.module.main.service;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import kr.co.maple.common.model.ResDTO;
+import kr.co.maple.common.service.WebClientService;
 import kr.co.maple.module.main.model.RankingListDTO;
 import lombok.RequiredArgsConstructor;
 
@@ -25,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MainService {
 	private static final Logger log = LogManager.getLogger("kr.co.maple");
+	private final WebClientService webClientServce;
 	@Value("${maple.apiKey}")
 	private String API_KEY;
 	@Value("${maple.url}")
@@ -36,24 +31,27 @@ public class MainService {
 	    params.add("date", "2023-12-22");
 	    params.add("world_type", "0");
 	    
-	    RankingListDTO rankingDTOList = WebClient.create(BASE_URL + "/maplestory/v1/ranking/overall")
-								.get()
-								.uri(uriBuilder ->
-										uriBuilder.queryParams(params).build()
-								)
-								.header("x-nxopen-api-key", API_KEY)
-								.accept(MediaType.APPLICATION_JSON)
-				                .retrieve()
-					            .onStatus(HttpStatus::is4xxClientError, response -> {
-					            	log.info(response.statusCode()  + " 에러");
-					            	throw new RuntimeException("Client Error");
-					            })
-								.onStatus(HttpStatus::is5xxServerError, response -> {
-									log.info(response.statusCode()  + " 에러");
-									throw new RuntimeException("Internal Server Error");
-								})
-				                .bodyToMono(RankingListDTO.class)			          
-				           		.block(); 
+	    
+	    RankingListDTO rankingDTOList  = webClientServce.webClientGetApi(BASE_URL + "/maplestory/v1/ranking/overall", 
+	    		params, API_KEY, RankingListDTO.class);
+//	    RankingListDTO rankingDTOList = WebClient.create(BASE_URL + "/maplestory/v1/ranking/overall")
+//								.get()
+//								.uri(uriBuilder ->
+//										uriBuilder.queryParams(params).build()
+//								)
+//								.header("x-nxopen-api-key", API_KEY)
+//								.accept(MediaType.APPLICATION_JSON)
+//				                .retrieve()
+//					            .onStatus(HttpStatus::is4xxClientError, response -> {
+//					            	log.info(response.statusCode()  + " 에러");
+//					            	throw new RuntimeException("Client Error");
+//					            })
+//								.onStatus(HttpStatus::is5xxServerError, response -> {
+//									log.info(response.statusCode()  + " 에러");
+//									throw new RuntimeException("Internal Server Error");
+//								})
+//				                .bodyToMono(RankingListDTO.class)			          
+//				           		.block(); 
 		System.out.println(rankingDTOList);			
 					
 		return new ResponseEntity<>(
